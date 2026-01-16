813583c67bbd6a4120327e23a6d3ae3a90840e84
shreedhi05 <shreedhi05@gmail.com>
2026-01-16 20:27:10 +0530

    day18

---
+++ day18.py
@@ -0,0 +1,110 @@
+#loop
+# Loop / repetition statement
+
+# for ( when we know the iteration starting and ending (range) )
+
+# syntax
+# for variable_name in range(start, end+1, steps):
+#     code
+
+
+# while ( condition base )
+
+# syntax
+# variable_name = starting point
+# while condition:
+#     code
+#     increment/decrement
+
+
+# print value from 1 to 10
+
+# for val in range(1, 11, 1):
+    # print(val,end=" ")
+
+# val=1
+# while val <=10
+#     print(val,end=" ")
+#     val +=1
+
+# print (val)
+
+
+#print 10 to 1
+
+
+# while ( condition base )
+# syntax
+# variable_name = starting point
+# while condition:
+#     code
+#     increment/decrement
+
+
+#print value from 1 to 20
+# val=1
+# while val <=20:
+#      print(val,end=" ")
+    #  val +=1
+
+# print(val)
+
+# for val in range(1,21,1):
+#     print(val,end=" ")
+
+
+#print value from 10 to 1
+# for i in range (11,0,-1):
+#     print(i,end=" ")
+
+# val=10
+# while val>=1:
+#     print(val,end=" ")
+#     val-=1
+
+#print value from 1 to n/
+# n=int(input("enter a no"))
+# for val in range(1,n,1):
+#     print(val,end=" ")
+
+# n=int(input("enter number"))
+# val=1
+# while val<=n:
+#     print(val,end=" ")
+#     val+=1
+
+#print value from n to 1
+# n=int(input("enter a no"))
+# for val in range(n,1,-1):
+#      print(val,end=" ")
+
+# n=int(input("enter number"))
+# while n>=1:
+#      print(n,end=" ")
+#      n-=1
+
+#print even no from 1 to 10
+
+# for i in range(1,11,1):
+#     if i % 2 == 0:
+#         print(i,end=" ") 
+
+# val=1
+# while val<=10:
+#   if val % 2 == 0:
+#     print(val,end=" ")
+#   val+=1
+
+#print odd no from 1 to 10
+# for i in range(1,11,1):
+#     if i % 2!= 0:
+#         print(i,end=" ") 
+
+
+# n=int(input("enter number"))
+# i=1
+# while i<=n:
+#     if i % 2!=0:
+#         print(i,end=" ")
+#     i+=1    
+
